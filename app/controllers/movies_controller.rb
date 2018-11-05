e170cf4dd834f210d9a6dde2fa004e85a99ed866
Trang <trang.frego@gmail.com>
2018-11-05 14:38:51 -0800

    create Movie index and show actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,21 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+
+    render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
 
   def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie.nil?
+      render json: {
+        message: 'not found'
+      }, status: :not_found
+    else
+      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory]), status: :ok
+    end
+
   end
 
   def create
