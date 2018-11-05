3c388a4b99a99d5fa52c15b26bbe02adcf0ba4ff
Divya <divyamohanan@gmail.com>
2018-11-05 13:03:08 -0800

    Added validations for Customer model, presence:true

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  validate :name, presence: true
+  validate :registered_at, presence: true
+  validate :address, presence: true
+  validate :city, presence: true
+  validate :state, presence: true
+  validate :postal_code, presence: true
+  validate :phone, presence: true
 end
