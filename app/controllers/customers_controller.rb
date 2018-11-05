2d6fff770911b8bdb6c5d202303df9b9c1c5ef12
Divya <divyamohanan@gmail.com>
2018-11-05 14:36:13 -0800

    Added contoller index method for Customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,6 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:name, :registered_at, :address, :city, :state, :postal_code, :phone])
+  end
 end
