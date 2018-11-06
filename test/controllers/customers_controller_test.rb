f46cd6b1f3a2f7fc07d921c44a6bb0cd7f63f3ad
Divya <divyamohanan@gmail.com>
2018-11-06 11:14:31 -0800

    Tests for customer controller and create method in movies controller

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+ describe "tests the index method" do
+  it "returns an Array" do
+    # Act
+    get customers_path
+
+    # Convert the JSON response into a Hash
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body).must_be_kind_of Array
+  end
+
+  it "returns all of the customers" do
+    # Act
+    get customers_path
+
+    # Convert the JSON response into a Hash
+    body = JSON.parse(response.body)
+
+    # Assert
+    expect(body.length).must_equal Customer.count
+  end
+
+  it "returns customers with exactly the required fields" do
+    keys = %w(id name phone postal_code registered_at)
+
+    # Act
+    get customers_path
+
+    # Convert the JSON response into a Hash
+    body = JSON.parse(response.body)
+
+    # Assert that each
+    body.each do |customer|
+      expect(customer.keys.sort).must_equal keys
+      expect(customer.keys.length).must_equal keys.length
+    end
+  end
+end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,19 +1,47 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
-  end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
-  end
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "The Conjuring",
+        overview: "Horror",
+        release_date: "2015-10-02",
+        inventory: "4"
+      }
+    }
 
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
-  end
+    it "creates a new movie given valid data" do
+      expect {
+      post movies_path, params: { movie: movie_data }
+    }.must_change "Movie.count", 1
 
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      movie = Movie.find(body["id"].to_i)
+
+      expect(movie.title).must_equal movie_data[:title]
+      must_respond_with :success
+    end
+
+    it "returns an error for invalid movie data" do
+      # arrange
+      movie_data["title"] = nil
+
+      expect {
+      post movies_path, params: { movie: movie_data }
+    }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+    end
+
+  end
 end
