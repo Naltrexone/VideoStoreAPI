d1827c185d29fa497d2ba5b2a32cf3b21eb05e38
Trang <trang.frego@gmail.com>
2018-11-06 13:46:48 -0800

    pass smoke tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,6 @@ class MoviesController < ApplicationController
   private
 
   def movie_params
-    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+    params.permit(:title, :overview, :release_date, :inventory)
   end
 end
