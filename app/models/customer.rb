90ddd41f3273eb2d47d33d806ad8c03b689b7640
Divya <divyamohanan@gmail.com>
2018-11-06 21:06:39 -0800

    Tests for Edamam wrapper method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,4 +8,18 @@ class Customer < ApplicationRecord
   validates :phone, presence: true
 
   has_many :rentals
+
+  def movies_checked_out_count
+    total = 0
+    num_out = 0
+    num_in = 0
+    self.rentals.each do |rental|
+     if rental.status == "out"
+       num_out += 1
+     elsif rental.status == "in"
+       num_in += 1
+     end
+    end
+    return (movies_count = num_out - num_in)
+ end
 end
