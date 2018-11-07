a9a8f7a9eb6d13849adc881253f895360459ab80
Divya <divyamohanan@gmail.com>
2018-11-07 13:11:59 -0800

    Tests for rentals model.. custom methods

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -6,4 +6,26 @@ describe Rental do
   it "must be valid" do
     value(rental).must_be :valid?
   end
+
+  describe "it tests the method that checks if movie is available" do
+    it "checks if no movie is available when all are checked out" do
+      movie = movies(:scream)
+      answer = rental.movie_avail?(movie.id)
+
+      expect(answer).must_equal false
+
+    end
+
+    it "checks that movie is available when there are more in inventory than are checked out" do
+      movie = movies(:titanic)
+      answer = rental.movie_avail?(movie.id)
+      expect(answer).must_equal true
+    end
+
+    it "checks that movie is not available when there are none in inventory" do
+      movie = movies(:beemovie)
+      answer = rental.movie_avail?(movie.id)
+      expect(answer).must_equal false
+    end
+  end
 end
